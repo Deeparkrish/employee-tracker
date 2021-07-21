535af4919a9cfc6b09865a85c1add1650185fab2
Deepa <kosalai17@gmail.com>
2021-07-21 11:48:48 -0700

    added basic inquirer

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,5 @@
-INSERT INTO department(dept_name)
+USE employees;
+INSERT INTO department(name)
 VALUES
 ('Marketing/Sales'),
 ('Human Resources'),
@@ -7,20 +8,21 @@ VALUES
 ('Customer Support'),
 ('Engineering');
 
-INSERT INTO roles (title, salary,department_id)
+ INSERT INTO role(title, salary,department_id)
 VALUES
-('Manager',10,000.99,1),
-('Lead Engineer',6999.99,3),
-('Accountant',8999.99,4),
-('Specialist',9999.99,5)
-('Admin',8999.99,4),
-('Intern',5999.99,2);
+('Manager',10000,1),
+('Lead Engineer',6999,3),
+('Accountant',8999,4),
+('Specialist',9999,5),
+('Admin',8999,2),
+('Intern',5999,2);
 
 INSERT INTO employee (first_name, last_name,role_id,manager_id)
 VALUES
-('Ronald', 'Firbank', 1, NULL),
-  ('Virginia', 'Woolf', 1, 1),
-  ('Piers', 'Gaveston', 4, NULL),
-  ('Charles', 'LeRoi', 2, 5),
-  ('Katherine', 'Mansfield', 2, 3),
-  ('Dora', 'Carrington', 3, NULL);
+('Ronald','Firbank',1,1),
+  ('Virginia','Woolf',1,1),
+  ('Piers','Gaveston',4,NULL),
+  ('Charles','LeRoi',2,1),
+  ('Katherine','Mansfield',2,1),
+  ('Dora','Carrington',3,NULL);
+ 
--- server.js
+++ server.js
@@ -24,7 +24,85 @@ console.log(``);
 console.log(chalk.yellow.bold(`====================================================================================`));
 
 function promptUserInput (){
+    inquirer
+    .prompt([
+        {
+          name: 'choices',
+          type: 'list',
+          message: 'Please select an option:',
+          choices: [
+            'View All Employees',
+            'View All Employees By Department',
+            'View All Employees By Role',
+            'View All Employees By Manager',
+            'View All Employees by Order of Salary',
+            'Add Employee',
+            'Remove Employee',
+            'Update Employee Role',
+            'Update Employee Manager',
+            'View All Roles',
+            'Add Role',
+            'Remove Role',
+            'View All Departments',
+            'Add Department',
+            'Remove Department',
+            'Exit'
+            ]
+        }
+    ])
+  .then(answer => {
+        switch(answer.action){
+            case "View All Employees":
+                //viewAllEmp();
+                break;
+            case "View all Employees By Department":
+                //viewAllEmpByDept();
+                break;
+
+            case "View all Employees By Role":
+                //viewAllEmpByRole();
+                break;
+
+            case "View all Employees By Manager":
+                ///viewAllEmpByMgr();
+                break;
+            case "Add Employee":
+                //addEmp();
+                break;
+            case "Remove Employee":
+                //removeEmp();
+                break;
+            case "Update Employee Role":
+                //updateEmpRole();
+                break;
+            case "Update Employee Manager":
+                //updateEmpMgr();
+                break;
+            case "View all Roles":
+                //viewAllRoles();
+                break;
+            case "Add Role":
+                //addRole();
+                break;
+            case "Remove role":
+                //removeRole();
+                break;
+            case "View all Departments":
+                    //viewDeptBudget();
+                    break;
+            case "Add Department":
+                //addDept();
+                break;
+            case "Remove department":
+                //removeDept();
+                break;
+            case "Exit":
+                db.end(); 
+                break;
+            
+
+        }
+
+    });
     
-
-
-}
+};
