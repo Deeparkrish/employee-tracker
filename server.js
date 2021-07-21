dbd05b2b9ba71d527aabccfba68f95b1d1e22508
Deepa <kosalai17@gmail.com>
2021-07-21 01:07:31 -0700

    added banner

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const db = require('./db/connection'); // connection to database 
 const inquirer = require('inquirer') 
 const figlet = require('figlet');
+const chalk = require('chalk');
 
 // Connect to Database
 db.connect(err =>{
@@ -9,12 +10,21 @@ db.connect(err =>{
     else {
     console.log('Database connected.');
     // call inquirer prompt 
+    promptUserInput();
     }
 }); 
 
 //This line of code runs a synchronous function through the figlet npm that displays the designated text string in the console
-console.log(figlet.textSync('Employee Management', {
-    font: 'Standard',
-    horizontalLayout: 'default',
-    verticalLayout: 'default'
-}));
+console.log(chalk.yellow.bold(`====================================================================================`));
+console.log(``);
+console.log(chalk.greenBright.bold(figlet.textSync('Employee-Tracker')));
+console.log(``);
+console.log(``);                                                        
+console.log(``);
+console.log(chalk.yellow.bold(`====================================================================================`));
+
+function promptUserInput (){
+    
+
+
+}
